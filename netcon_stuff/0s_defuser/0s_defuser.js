8036e26b0e1cf7b99db1a4583baefd4d66b002cb
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2020-08-17 17:49:42 +0200

    Improve `0s_defuser`

--- netcon_stuff/0s_defuser/0s_defuser.js
+++ netcon_stuff/0s_defuser/0s_defuser.js
@@ -3,87 +3,122 @@ const net = require('net');
 const readline = require('readline');
 const path = require('path');
 
-// Change this to 2 if it fails for you
-// This should NOT be greater than 6
-const pingMultiplier = 1.5;
+const {now} = Date;
+const whitespaceRegExp = /\s{2,}/g;
 
-const TICK_RATE = 64;
-const PING_DELIMITER = ' ms : ';
+const config = {
+	pingMultiplier: 1,
 
-let previous = {};
+	// If you get 0.02 instead of 0.000, you can try setting this to -10.
+	// If it doesn't help, change the `pingMultiplier` to 0.5 or lower.
+	pingOffset: 0,
+
+	ms: {
+		tick: Math.ceil(1000 / 64),
+		timeoutInaccuracy: 4,
+		inputLag: 4,
+		defuse: 10000,
+		defuseKit: 5000,
+		c4: 40000,
+		ping: 0
+	},
+
+	username: '',
+	defuseMode: 0
+};
+
+const previousNetStats = {};
+
+const constants = {
+	pingDelimiter: ' ms : ',
+	net_dumpeventstats: {
+		types: {
+			Name: 'string',
+			Out: 'number',
+			In: 'number',
+			OutBits: 'number',
+			InBits: 'number',
+			OutSize: 'number',
+			InSize: 'number',
+			Notes: 'string'
+		}
+	},
+	name: {
+		startsWith: '"name" = "',
+		endsWith: '" ( def. "unnamed" ) archive server_can_execute user ss'
+	},
+	autodefuse: {
+		timeUntilExplosion: 'Time until explosion',
+		0: '[NO AUTODEFUSE]',
+		5: '[AUTODEFUSE 5s]',
+		10: '[AUTODEFUSE 10s]'
+	}
+};
+
+constants.net_dumpeventstats.keys = Object.keys(constants.net_dumpeventstats.types);
+constants.net_dumpeventstats.string = constants.net_dumpeventstats.keys.join(' ');
+
 let timeout10s;
 let timeout5s;
-let counter = 0;
 
-let defuseMode = 0;
 let explosionTime = 0;
-let ping = 1;
-let username = 'unnamed';
 
 const sendOnConnect = `developer 1;
 con_filter_enable 2;
 con_filter_text "Time until explosion: ";
 bind 0 "echo defuse_0s_0";
-bind - "echo defuse_0s_1";
-bind = "echo defuse_0s_2";`;
+bind - "echo defuse_0s_5";
+bind = "echo defuse_0s_10";
+name;
+`;
 
 const onChange = data => {
-	const hasPrevious = data.Name in previous;
-	const isDifferent = !hasPrevious || previous[data.Name].In !== data.In;
+	const hasPrevious = data.Name in previousNetStats;
 
-	if (!isDifferent) {
+	if (!hasPrevious) {
+		previousNetStats[data.Name] = data;
 		return;
 	}
 
+	if (previousNetStats[data.Name].In === data.In) {
+		return;
+	}
+
+	previousNetStats[data.Name] = data;
+
 	if (data.Name === 'bomb_planted') {
-		previous[data.Name] = data;
-
-		if (counter === 2) {
-			explosionTime = Date.now() + 40000 - pingMultiplier * ping - 1;
-			clearTimeout(timeout10s);
-			clearTimeout(timeout5s);
-			
-			timeout10s = setTimeout(() => {
-				if (defuseMode === 1) {
-					socket.write(`+use\n`);
-				}
-			}, 30000 - pingMultiplier * ping - 1);
-			
-			timeout5s = setTimeout(() => {
-				if (defuseMode === 2) {
-					socket.write(`+use\n`);
-				}
-			}, 30000 - pingMultiplier * ping - 1 + 5000);
+		if (explosionTime !== 0) {
+			return;
 		}
+
+		const explodesInMs = config.ms.c4 - config.ms.tick - config.pingMultiplier * config.ms.ping - config.pingOffset;
+
+		explosionTime = now() + explodesInMs;
+
+		const delay = explodesInMs - config.ms.defuse - config.ms.timeoutInaccuracy - config.ms.inputLag;
+
+		timeout10s = setTimeout(() => {
+			if (config.defuseMode === 10) {
+				socket.write('+use\n');
+			}
+		}, delay);
+
+		timeout5s = setTimeout(() => {
+			if (config.defuseMode === 5) {
+				socket.write('+use\n');
+			}
+		}, delay + config.ms.defuse - config.ms.defuseKit);
 	} else if (data.Name === 'round_start') {
-		previous[data.Name] = data;
+		config.defuseMode = 0;
 
-		if (counter === 2) {
-			clearTimeout(timeout10s);
-			clearTimeout(timeout5s);
-			explosionTime = 0;
-			defuseMode = 0;
-			socket.write(`-use\n`);
-		}
+		clearTimeout(timeout10s);
+		clearTimeout(timeout5s);
+
+		explosionTime = 0;
+
+		socket.write('-use\n');
 	}
 };
-
-const COMMAND = 'net_dumpeventstats';
-const WANTED = {
-	Name: 'string',
-	Out: 'number',
-	In: 'number',
-	OutBits: 'number',
-	InBits: 'number',
-	OutSize: 'number',
-	InSize: 'number',
-	Notes: 'string'
-};
-
-const WANTED_KEYS = Object.keys(WANTED);
-const WANTED_STRING = WANTED_KEYS.join(' ');
-const WANTED_LENGTH = WANTED_KEYS.length;
-const whitespaceRegExp = /\s{2,}/g;
 
 const port = Number(process.argv[2] || 0);
 if (process.argv.length !== 3 || !port) {
@@ -94,8 +129,28 @@ if (process.argv.length !== 3 || !port) {
 const socket = net.connect(port, '127.0.0.1', async () => {
 	console.log('Connected! Press CTRL+C to abort.');
 
-	socket.write(`${sendOnConnect}\nname\n`);
+	// Initialize
+	socket.write(sendOnConnect);
+	
+	// Get ping, display time
+	{
+		let lastPingTime = 0;
 
+		setInterval(() => {
+			const timeUntilExplosion = (Math.max(explosionTime - now(), 0) / 1000).toFixed(3);
+
+			let pingCommand = '';
+			const ms = Date.now();
+			if ((ms - lastPingTime) > 100) {
+				lastPingTime = ms;
+				pingCommand = 'ping\n';
+			}
+
+			socket.write(`clear\n${pingCommand}net_dumpeventstats\necho "${constants.autodefuse.timeUntilExplosion}: ${timeUntilExplosion}s ${constants.autodefuse[config.defuseMode]}"\n`);
+		}, config.ms.tick).unref();
+	}
+
+	// Read data
 	const reader = readline.createInterface({
 		input: socket,
 		crlfDelay: Infinity
@@ -105,86 +160,75 @@ const socket = net.connect(port, '127.0.0.1', async () => {
 
 	for await (const line of reader) {
 		const processedLine = line.replace(whitespaceRegExp, ' ').trim();
-		
-		if (processedLine.startsWith(`"name" = "`)) {
-			const end = `" ( def. "unnamed" ) archive server_can_execute user ss`;
-			username = processedLine.slice(10, processedLine.indexOf(end));
-		} else if (processedLine.includes(PING_DELIMITER)) {
-			const data = processedLine.split(PING_DELIMITER);
-			
-			if (data[1] === username) {
-				ping = Number(data[0]);
+
+		// Process username
+		if (processedLine.startsWith(constants.name.startsWith)) {
+			config.username = processedLine.slice(constants.name.startsWith.length, processedLine.indexOf(constants.name.endsWith));
+			continue;
+		}
+
+		// Process ping
+		if (processedLine.includes(constants.pingDelimiter)) {
+			const data = processedLine.split(constants.pingDelimiter);
+
+			if (data[1] === config.username) {
+				config.ms.ping = Number(data[0]);
 			}
-		} if (processedLine === 'defuse_0s_0') {
-			defuseMode = 0;
-		} else if (processedLine === 'defuse_0s_1') {
-			defuseMode = 1;
-		} else if (processedLine === 'defuse_0s_2') {
-			defuseMode = 2;
-		} else {
-			if (reading) {
-				const parsedLine = processedLine.split(' ');
 
-				if (parsedLine.length !== WANTED_LENGTH) {
-					reading = false;
-					continue;
-				}
+			continue;
+		}
 
-				const object = {};
+		// Update defuse mode
+		if (processedLine === 'defuse_0s_0') {
+			config.defuseMode = 0;
+			continue;
+		} else if (processedLine === 'defuse_0s_5') {
+			config.defuseMode = 5;
+			continue;
+		} else if (processedLine === 'defuse_0s_10') {
+			config.defuseMode = 10;
+			continue;
+		}
 
-				for (let index = 0; index < WANTED_KEYS.length; index++) {
-					const key = WANTED_KEYS[index];
-					const type = WANTED[key];
+		// Try to parse `net_dumpeventstats`
+		if (reading) {
+			const {string, types, keys} = constants.net_dumpeventstats;
 
-					if (type === 'string') {
-						object[key] = parsedLine[index];
-					} else if (type === 'number') {
-						object[key] = Number(parsedLine[index]);
+			const parsedLine = processedLine.split(' ');
+			if (parsedLine.length !== keys.length) {
+				reading = false;
+				continue;
+			}
 
-						if (Number.isNaN(object[key])) {
-							reading = false;
-						}
+			const object = {};
+
+			for (let index = 0; index < keys.length; index++) {
+				const key = keys[index];
+				const type = types[key];
+
+				if (type === 'string') {
+					object[key] = parsedLine[index];
+				} else if (type === 'number') {
+					object[key] = Number(parsedLine[index]);
+
+					if (Number.isNaN(object[key])) {
+						reading = false;
 					}
-				}
-
-				if (reading) {
-					onChange(object);
+				} else {
+					throw new Error(`Unknown value type: ${type}`);
 				}
 			}
 
-			if (processedLine === WANTED_STRING) {
-				if (counter < 2) {
-					counter++;
-				}
-
-				reading = true;
+			if (reading) {
+				onChange(object);
 			}
+		}
+
+		// We are gonna parse `net_dumpeventstats`
+		if (processedLine === constants.net_dumpeventstats.string) {
+			reading = true;
 		}
 	}
 });
-
-let lastPingTime = 0;
-
-setInterval(() => {
-	const diff = Math.max(explosionTime - Date.now(), 0);
-
-	let mode;
-	if (defuseMode === 0) {
-		mode = '[NO AUTODEFUSE]';
-	} else if (defuseMode === 1) {
-		mode = '[AUTODEFUSE 10s]';
-	} else if (defuseMode === 2) {
-		mode = '[AUTODEFUSE 5s]';
-	}
-
-	let pingCommand = '';
-	const now = Date.now();
-	if ((now - lastPingTime) > 100) {
-		lastPingTime = now;
-		pingCommand = 'ping\n';
-	}
-
-	socket.write(`clear\n${pingCommand}${COMMAND}\necho "Time until explosion: ${(diff / 1000).toFixed(3)}s ${mode}"\n`);
-}, Math.floor(1000 / TICK_RATE)).unref();
 
 socket.setEncoding('utf8');
